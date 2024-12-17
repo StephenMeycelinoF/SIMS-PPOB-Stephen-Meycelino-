57fd19914a98eac7a8f1454eba69973420b63e6f
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-12-17 18:21:56 +0700

    add: setup shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <main className="text-xl ">
       <h1 className="text-red-500">Hello</h1>
+      <Button>Hello</Button>
     </main>
   );
 }
