dff997d720afa6eb45d28b2d0b7b924c1fe51326
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-12-17 21:07:59 +0700

    fix: store file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { store } from "@/app/store";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
